7aac564a2a5de49cf79e6eb7c581044d74244420
dahuan <wangdahuan.@aliyun.com>
2023-12-24 18:35:53 +0800

    添加消息体和用户信息

---
+++ QQClient/src/qqcommon/User.java
@@ -0,0 +1,32 @@
+package qqcommon;
+
+import java.io.Serializable;
+
+/**
+ * 用户信息
+ */
+public class User implements Serializable {
+    private String userId;//用户名
+    private String passward;//用户密码
+
+    public User(String userId, String passward) {
+        this.userId = userId;
+        this.passward = passward;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassward() {
+        return passward;
+    }
+
+    public void setPassward(String passward) {
+        this.passward = passward;
+    }
+}
